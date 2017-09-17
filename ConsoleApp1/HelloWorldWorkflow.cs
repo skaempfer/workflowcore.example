3e9b539bbea747412eba87c844229612e286e996
Sebastian Kämpfer <sebastian.kaempfer@gmx.net>
2017-09-17 20:10:40 +0200

    Initial commit.

---
+++ ConsoleApp1/HelloWorldWorkflow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+
+namespace ConsoleApp1
+{
+    public class HelloWorldWorkflow : IWorkflow
+    {
+        public string Id => "HelloWorld";
+
+        public int Version => 1;
+
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder
+                .StartWith<HelloWorld>()
+                .Then<GoodbyeWorld>();
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using WorkflowCore.Interface;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceProvider serviceProvider = ConfigureServices();
+
+            //start the workflow host
+            var host = serviceProvider.GetService<IWorkflowHost>();
+            host.RegisterWorkflow<HelloWorldWorkflow>();
+            host.Start();
+
+            host.StartWorkflow("HelloWorld", 1, null);
+
+            Console.ReadLine();
+
+            host.Stop();
+        }
+        private static IServiceProvider ConfigureServices()
+        {
+            //setup dependency injection
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            services.AddWorkflow(x => x.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=WorkflowCore;Trusted_Connection=True;", true, true));
+            //services.AddWorkflow(x => x.UseMongoDB(@"mongodb://localhost:27017", "workflow"));
+            services.AddTransient<HelloWorld>();
+            services.AddTransient<GoodbyeWorld>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            //config logging
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddDebug();
+            return serviceProvider;
+        }
+    }
+}
